edc4d68899c41d6f4c1efd6e3798eb0c0a10a2de
MathiasChirivi <chirivimathias3@gmail.com>
2023-07-10 19:15:45 +0200

    ese finito

--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -5,14 +5,16 @@
 <div class="container-fluid mt-4">
     <div class="row justify-content-between">
         @foreach ($projects as $project)
-            <div class="card p-0 mb-4" style="width: 18rem;">
-                <div class="card-body">
-                    <h5 class="card-title">{{ $project->title }}</h5>
-                    <p class="card-text">{{ $project->description }}</p>
-                    <p class="card-text">{{ $project->repository }}</p>
+        <div class="col-4">
+            <div class="card mt-3" style="height: 25rem;">
+                <div class="card-body h-25">
+                    <h5 class="card-title"><strong>Nome del progetto:</strong> {{ $project->title }}</h5>
+                    <p class="card-text"><strong>Descrizione del progetto:</strong> {{ $project->description }}</p>
+                    <p class="card-text"><strong>Nome repository:</strong> {{ $project->repository }}</p>
                 </div>
             </div>
-        @endforeach
+        </div>
+    @endforeach
     </div>
 </div>
 
